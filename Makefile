ac412779ac3a1e6ddd194a133d47a9c0935c54d5
nikolay-egorov <deathadder98@gmail.com>
2021-12-14 13:39:00 +0300

    Improve levels + switch to Cmake

--- Makefile
+++
@@ -1,2 +0,0 @@
-default: a.out
-	gcc test.cpp -std=c++14 -lstdc++ && ./a.out
--- test.cpp
+++ test.cpp
@@ -50,6 +50,7 @@ long Time() {
     auto startTime = std::chrono::high_resolution_clock::now();
     runLoop(1000000);
     auto endTime = std::chrono::high_resolution_clock::now();
+    //std::cout << "inside Time : " << (endTime - startTime).count() << '\n';
     return (endTime - startTime).count();
 }
 
@@ -92,41 +93,70 @@ bool sortFirst(const std::tuple<int, int>& lhs,const std::tuple<int, int>& rhs) 
 }
 
 
-long traverseCache(long size, int cacheLineSize) {
+size_t* prepareArray(long size, size_t cacheLineSize) {
+    auto* buf = new size_t[size]{0};
+
+    size_t chainedInd = 0;
+    size_t i = 1;
+    while (i++ < size) {
+        buf[chainedInd] = size;
+        size_t nextStop = (cacheLineSize * rand()) % size;
+        while (buf[nextStop] > 0) {
+            nextStop = (1 + nextStop) % size;
+        }
+        buf[chainedInd] = nextStop;
+        chainedInd = nextStop;
+    }
+    return buf;
+}
+
+long traverseL1Cache(long size, size_t cacheLineSize) {
     char* buf = new char[size];
-
     auto startTime = std::chrono::high_resolution_clock::now();
-    //auto startTime = clock();
-
-    // replace with cache-line size
     for (size_t i = 0; i < 64 * MB; i++){
         ++buf[(i * cacheLineSize) % size]; // means we write to a new cache-line (I hope so)
     }
-
-    //auto endTime = clock();
     auto endTime = std::chrono::high_resolution_clock::now();
     delete [] buf;
 
     return (endTime - startTime).count();
-    //return (endTime - startTime);
+}
+
+long traverseCache(long size, size_t cacheLineSize) {
+    auto* buf = prepareArray(size, cacheLineSize);
+    size_t v = 0;
+    //auto startTime = std::chrono::high_resolution_clock::now();
+    auto startTime = std::chrono::steady_clock::now();
+
+    // repeat 10 times
+    for (size_t i = 0; i < 10 * size; i++) {
+        v = buf[v];
+    }
+    auto endTime = std::chrono::steady_clock::now();
+
+    delete [] buf;
+
+    return (endTime - startTime).count();
 }
 
 // looks ok
 void determineL1Size(std::vector<long>& ans) {
-    long maxDiff = 0;
+    long maxDiff = 1;
     long prevTime = 1;
     std::vector<std::pair<long, long>> l1Probes;
     long maxDiffSize = 1024;
     // run separate loop for L1
-    for (long size = 1024; size <= L1SizeBound * 1024; size += 16 * 1024) {
+    //for (long size = 16 * 1024; size <= L1SizeBound * 1024; size += 16 * 1024) {
+    for (long size = 8 * 1024; size <= L1SizeBound * 1024; size *= 2) {
         auto currTime = traverseCache(size, 64);
         auto currDiff = std::abs(currTime - prevTime);
         //std::cout << size/1024 << ' ' << currTime << ' '  << currDiff << ' ' <<  '\n';
         if (!l1Probes.empty()) {
             auto prevDiff = l1Probes.back().first;
             auto relDiff = currDiff / prevDiff;
+            //std::cout << size/1024 << ' ' << currDiff << ' '  << relDiff << ' ' << maxDiff << '\n';
             if (relDiff > maxDiff) {
-                maxDiff = prevDiff;
+                maxDiff = currDiff;
                 maxDiffSize = size / 1024;
             }
         }
@@ -135,7 +165,7 @@ void determineL1Size(std::vector<long>& ans) {
         prevTime = currTime;
     }
 
-    std::cout << maxDiffSize << '\n';
+    std::cout << "L1 size: " <<  maxDiffSize << '\n';
     ans.push_back(maxDiffSize - 1);
 }
 
@@ -146,9 +176,9 @@ std::vector<long> determineCacheSizes() {
     std::vector<long> tmp;
 
     long minTime = 1;
-    auto step = 256 * 1024;
+    auto step = 1024 * 1024;
     // consider step for cache change like 256kb since first occurrence
-    long maxDiff = 0;
+    long maxDiff = 1;
     long prevTime = 1;
 
     determineL1Size(tmp);
@@ -156,24 +186,34 @@ std::vector<long> determineCacheSizes() {
     long startSize = 512 * 1024;
     // L2 and L3; 4096 for L2
     //std::vector<long> cacheBound{static_cast<long>(pow(2, 12)), 9200};
-    std::vector<long> cacheBound{static_cast<long>(pow(2, 12)), static_cast<long>(pow(2, 14))};
+    std::vector<long> cacheBound{static_cast<long>(pow(2, 12)), static_cast<long>(pow(2, 16))};
     std::vector<std::pair<long, long>> probes;
     long cacheS = 1;
+    bool foundCache = false;
+    float criticalBorderForCaches[] = {3, 2.2};
+    size_t cacheLvl = 0;
 
     for (const auto &item : cacheBound) {
-        std::cout << "Seek from " <<startSize/1024 << " to " << item * 1024 << '\n';
+        std::cout << "Seek from " << startSize/1024 << " to " << item << '\n';
         probes.clear();
-        for (long size = startSize; size <= item * 1024 - 1; size += step) {
-            auto currTime = traverseCache(size, 64);
+        for (long size = startSize; size <= item * 1024 + 1; size *= 2) {
+            if (foundCache) break;
+            auto currTime = traverseCache(size, 64) / 10; // should divide by 10?
             auto currDiff = std::abs(currTime - prevTime);
             if (size == startSize) {
                 prevTime = currTime;
+                probes.emplace_back(currDiff, size/1024);
+                maxDiff = currDiff;
                 continue;
             }
             if (!probes.empty()) {
-                if (currDiff > maxDiff) {
+                //if (currDiff > maxDiff) {
+                if (maxDiff == 1) {
+                    maxDiff = currDiff;
+                } else if ((currDiff + 0.1) / maxDiff > criticalBorderForCaches[cacheLvl] && !foundCache) { // experienced even 7 here at cache border
                     maxDiff = currDiff;
                     cacheS = size / 1024;
+                    foundCache = true;
                 }
             }
             probes.emplace_back(currDiff, size/1024);
@@ -183,7 +223,9 @@ std::vector<long> determineCacheSizes() {
         startSize = item * 1024;
         std::cout << "Potential cache is " << cacheS << '\n';
         tmp.push_back(cacheS);
-        maxDiff = 1;
+        maxDiff = cacheS = 1;
+        foundCache = false;
+        cacheLvl++;
     }
 
 
@@ -213,6 +255,7 @@ int main() {
         S = 2;
         while (S < N) {
             cur_time = Time();
+            //std::cout << "cur_time: " << cur_time << '\n';
             if (DeltaDiff()) {
                 jmpC++;
                 // need to do something with S
